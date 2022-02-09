a77168841599503be553b98ec70a90f96fafc889
Alin Andersen <alin.porcic@gmail.com>
2022-02-09 22:13:38 +0100

    Updates.

--- BuildPushNuget.sh
+++ BuildPushNuget.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 rm -rf AlinSpace.Database/bin/Release/*
-rm -rf AlinSpace.Database.Ef/bin/Release/*
 
 dotnet build -c Release
-
 dotnet nuget push AlinSpace.Database/bin/Release/AlinSpace.Database.*.nupkg --source github --skip-duplicate
-dotnet nuget push AlinSpace.Database.Ef/bin/Release/AlinSpace.Database.Ef.*.nupkg --source github --skip-duplicate
